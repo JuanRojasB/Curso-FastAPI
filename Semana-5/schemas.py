b32877cf1653725331c70999e12a5723ee6a4285
JuanRojasB <199319087+JuanRojasB@users.noreply.github.com>
2025-09-01 13:41:17 -0500

    feat(semana5): teoría de autenticación básica

---
+++ Semana-5/schemas.py
@@ -0,0 +1,16 @@
+# schemas.py
+from pydantic import BaseModel, EmailStr
+
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: str
+    is_active: bool
+
+    class Config:
+        from_attributes = True
